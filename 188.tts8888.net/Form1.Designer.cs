6903c7b4c3b18d7dc80e7dbea611de7fa4377b6b
howard <howard@2be.com.tw>
2012-11-22 14:50:08 +0800

    Add tts8888 new project
    Signed-off-by: howard <howard@2be.com.tw>

---
+++ 188.tts8888.net/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _188.tts8888.net
+{
+    partial class tts8888Form
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tts8888WebBrowser = new System.Windows.Forms.WebBrowser();
+            this.timerone = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tts8888WebBrowser
+            // 
+            this.tts8888WebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tts8888WebBrowser.IsWebBrowserContextMenuEnabled = false;
+            this.tts8888WebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.tts8888WebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.tts8888WebBrowser.Name = "tts8888WebBrowser";
+            this.tts8888WebBrowser.ScriptErrorsSuppressed = true;
+            this.tts8888WebBrowser.Size = new System.Drawing.Size(1071, 570);
+            this.tts8888WebBrowser.TabIndex = 1;
+            this.tts8888WebBrowser.TabStop = false;
+            this.tts8888WebBrowser.Url = new System.Uri("", System.UriKind.Relative);
+            this.tts8888WebBrowser.WebBrowserShortcutsEnabled = false;
+            this.tts8888WebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.tts8888WebBrowser_DocumentCompleted);
+            this.tts8888WebBrowser.NewWindow += new System.ComponentModel.CancelEventHandler(this.tts8888WebBrowser_NewWindow);
+            // 
+            // tts8888Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 570);
+            this.Controls.Add(this.tts8888WebBrowser);
+            this.Name = "tts8888Form";
+            this.ShowIcon = false;
+            this.Text = "TTS8888";
+            this.Load += new System.EventHandler(this.tts8888Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser tts8888WebBrowser;
+        private System.Windows.Forms.Timer timerone;
+    }
+}
+
---
+++ 188.tts8888.net/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using IniParser;
+
+namespace _188.tts8888.net
+{
+    public partial class tts8888Form : Form
+    {
+        public tts8888Form()
+        {
+            InitializeComponent();
+        }
+        private static string URL = "http://173.192.127.5:5021/company/tw/login.asp";
+        private void tts8888Form_Load(object sender, EventArgs e)
+        {
+            this.tts8888WebBrowser.Url = new System.Uri(URL, System.UriKind.Absolute);
+        }
+
+        private void tts8888WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (tts8888WebBrowser.ReadyState == WebBrowserReadyState.Complete)return;
+
+            FileIniDataParser parser = new FileIniDataParser();
+            IniData parserData = parser.LoadFile("C:/test.ini");
+
+            KeyDataCollection Keycol = parserData["Info"];
+            string iniUsername = Keycol["user"];
+            string iniPassWord = Keycol["pass"];
+
+            HtmlWindow windows = tts8888WebBrowser.Document.Window;
+            var inputTagName = windows.Document.GetElementsByTagName("input");
+            if (inputTagName.Count == 3) {
+                if (inputTagName[0] == null || inputTagName[1] == null || inputTagName[2] == null) {
+                    return;
+                } else {
+                    inputTagName[0].SetAttribute("value", iniUsername);
+                    inputTagName[2].SetAttribute("value", iniPassWord);
+                    inputTagName[1].InvokeMember("click");
+                }
+            }
+        }
+
+        private void tts8888WebBrowser_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}  
+
+  
